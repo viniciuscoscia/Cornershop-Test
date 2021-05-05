4a30d233ba3dd0e1088843db1c8b494782ecac46
Vinicius Coscia <vinicius.coscia@live.com>
2021-05-04 22:08:48 -0300

    Adição do moshi Avanços com layout

---
+++ app/src/main/java/com/cornershop/counterstest/data/ErrorResponse.kt
@@ -0,0 +1,6 @@
+package com.cornershop.counterstest.data
+
+data class ErrorResponse(
+    val message: String,
+    val code: Int
+)
---
+++ app/src/main/java/com/cornershop/counterstest/data/ResultWrapper.kt
@@ -0,0 +1,9 @@
+package com.cornershop.counterstest.data
+
+sealed class ResultWrapper<out T> {
+    data class Success<out T>(val value: T) : ResultWrapper<T>()
+    data class GenericError(val code: Int? = null, val error: ErrorResponse? = null) :
+        ResultWrapper<Nothing>()
+
+    object NetworkError : ResultWrapper<Nothing>()
+}
