4e21d36e23795be38b4c26b82d78668f566dcfe2
Vinicius Coscia <vinicius.coscia@live.com>
2021-05-04 21:16:35 -0300

    Criação do MainFragment.kt, criação de layout, diversas pequenas alterações

---
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.cornershop.counterstest.presentation.ui.main
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cornershop.counterstest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+}
--- app/src/main/java/com/cornershop/counterstest/presentation/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/welcome/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package com.cornershop.counterstest.presentation.ui.welcome
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.cornershop.counterstest.R
+import com.cornershop.counterstest.presentation.ui.main.MainActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        findViewById<Button>(R.id.buttonStart).setOnClickListener {
+            startActivity(MainActivity.newIntent(this))
+        }
     }
 }
